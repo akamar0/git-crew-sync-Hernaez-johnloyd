49a67bfcdf993056200d3a39e7c24def059339ab
Lenovo <loydhernaez@gmail.com>
2026-09-21 21:55:07 +0800

    SsRound overtime pay - Hernaez.johnloyd

--- shifts.js
+++ shifts.js
@@ -11,7 +11,5 @@ function calculatePay(hours, rate) {
   const overtimeHours = hours - 8;
   const overtimePay = overtimeHours * rate * 1.5;
 
-  return Math.round(regularPay + overtimePay);
+  return Math.ceil(regularPay + overtimePay);
 }
-
-module.exports = { isValidShift, calculatePay };
